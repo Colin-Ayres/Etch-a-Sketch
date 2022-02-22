65dee307e5ea7dceb6f159275ce053d01bff3cd0
Colin Ayres <CAyres7@gmail.com>
2022-02-21 19:04:47 -0500

    Added background for grid creation, added mouseover functionality

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-const container = document.querySelector('#container');
-let cols = document.getElementsByClassName("gridColumn");
-let square = document.getElementsByClassName("square");
+const container = document.querySelector(".grid-screen");
+const gamePad = document.querySelector(".game-pad");
+const resetButton = document.createElement("button");
+resetButton.classList.add('resetButton');
+resetButton.innerHTML = "Reset!";
+resetButton.type = "submit";
+resetButton.name = "formBtn";
+gamePad.insertBefore(resetButton, container)
+//gamePad.appendChild(resetButton);
 
-makeGrid(16,16); 
 
-function makeGrid(columnDepth, numRows) {
-    populateColumn(columnDepth); //Specifies number of elements per column
-    populateRow(numRows); //Specifies number of elements per row
+//Initialize grid-items with default size
+let size = 100;
+for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.setAttribute('class', 'grid-item');
+    div.setAttribute('id', 'div' + (i+1));
+    container.appendChild(div);
+}
+function resetGrid() {
+    gridItem.forEach((item) => {
+        document.getElementById(item.id).style.backgroundColor = "rgba(139, 194, 184, 0.952)";
+    });
 }
 
-function populateColumn(ColSize) {
-    for (r = 0; r < ColSize; r++) {
-        let col = document.createElement("div");
-        container.appendChild(col).className = "gridColumn";
-    };
-};
+resetButton.onclick = function () {
+    resetGrid();
+}
 
-function populateRow(rowSize) {
-    for (i = 0; i < rowSize; i++) {
-        for (j = 0; j < cols.length; j++) {
-            let newCell = document.createElement("div");
-            cols[j].appendChild(newCell).className = "square";
-        };
 
-    };
-};
+
+function randomColor (){
+     let color = '#' + Math.floor(Math.random()*16777215).toString(16);
+     return color;
+    }
+
+const gridItem=document.querySelectorAll(".grid-item");
+gridItem.forEach((item) => {
+    item.addEventListener('mouseover', function() {
+       
+            document.getElementById(item.id).style.backgroundColor = "black";
+        
+            
+        })
+})
